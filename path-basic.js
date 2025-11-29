f0bc4a08bdf86ed532a34ac8c5cd837b5db6c5e9
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-11-29 12:00:57 +0600

    File Organize

--- path-basic.js
+++ path-basic.js
@@ -5,3 +5,17 @@ console.log('filename: ',__filename);
 console.log('Directory:',__dirname);
 
 console.log('============')
+
+const filePath = '/anika/documents/nextLevel.pdf'
+
+console.log('analyze Path:', filePath,'\n')
+console.log('Directory:', path.dirname(filePath))
+console.log('Base name:', path.basename(filePath))
+console.log('File Extension', path.extname(filePath))
+console.log('File Name', path.basename(filePath,path.extname(filePath)))
+
+console.log('---------------------------')
+
+const parsed = path.parse(filePath)
+console.log('Parsed path object:',parsed)
+console.log('formatted path:', path.format(parsed))
